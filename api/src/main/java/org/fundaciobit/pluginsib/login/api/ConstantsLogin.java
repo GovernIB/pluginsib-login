2a39de6d1f2ccdcb3c41ad6c6c35f143476323e5
Antoni Nadal <anadal@fundaciobit.org>
2023-11-15 14:45:47 +0100

    Aïllar el PLugin de Login en un repositori propi   https://github.com/GovernIB/evidenciesib/issues/28

---
+++ api/src/main/java/org/fundaciobit/pluginsib/login/api/ConstantsLogin.java
@@ -0,0 +1,14 @@
+package org.fundaciobit.pluginsib.login.api;
+
+/**
+ * 
+ * @author anadal
+ *
+ */
+public interface ConstantsLogin {
+
+    public static final String TICKET_PARAM = "ticket";
+
+    public static final String SESSION_LOGIN_ERROR = "SESSION_LOGIN_ERROR";
+
+}
